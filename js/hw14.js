003fea170e3fe337960d6e1db0309d0bb5ee748c
kalyushkoartem <kalyushkoartem@gmail.com>
2026-05-06 19:50:02 +0300

    sdfm67

--- js/hw14.js
+++ js/hw14.js
@@ -96,12 +96,17 @@ const users = [
 const getUserNames = users => {
   return users.map(user => user.name);
 };
-
 console.log(getUserNames(users));
+
 
 const getUsersWithEyeColor = (users, color) => {
   return users.filter(user => user.eyeColor === color);
 };
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+
+
+
 
 const getUsersWithGender = (users, gender) => {
   return users
@@ -109,10 +114,20 @@ const getUsersWithGender = (users, gender) => {
     .map(user => user.name);
 };
 
+console.log(getUsersWithGender(users, 'male'));
+
+
+
+
 const getInactiveUsers = users => {
   return users.filter(user => !user.isActive);
 };
+console.log(getInactiveUsers(users));
+
 
 const getUserWithEmail = (users, email) => {
   return users.find(user => user.email === email);
 };
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+
